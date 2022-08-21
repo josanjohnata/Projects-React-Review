8d2206bd50927a6c3da9d0014a61fc77f4b0bb63
josanjohnata <josan.potes@meliuz.com.br>
2022-08-21 11:21:31 -0300

    feat: Adiciona componente Header

---
+++ to-do-list-project/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+
+import "./App.module.css";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+      </main>
+    </>
+  );
+}
+
+export default App;
